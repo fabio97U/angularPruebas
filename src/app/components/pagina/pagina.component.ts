a838b0ba4fece9c9367b73f26f501ddc6390dea9
Fabio Ernesto Ramos Reyes <fabio.ramos@outlook.es>
2021-10-15 21:51:32 -0600

    Agrego componentes

---
+++ src/app/components/pagina/pagina.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+
+@Component({
+  selector: 'app-pagina',
+  templateUrl: './pagina.component.html',
+  styleUrls: ['./pagina.component.css']
+})
+export class PaginaComponent implements OnInit {
+
+  public nombre: string;
+  constructor(private _route: ActivatedRoute, private _router: Router) {
+    this.nombre = "";
+  }
+
+  ngOnInit(): void {
+    this._route.params.subscribe((params: Params) => {
+      console.log(params);
+      this.nombre = params.nombre;
+    });
+  }
+
+}
